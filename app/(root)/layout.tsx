adc2cc55828a7a0ab5103bedfbe1b2c3e0dc2891
jmcauhy <jmcauhy@gmail.com>
2023-08-10 07:38:31 -0300

    Add base UI components to root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,10 @@
 import { ClerkProvider } from "@clerk/nextjs"
 import { Inter } from 'next/font/google'
 
+import TopBar from "@/components/shared/TopBar"
+import LeftSidebar from "@/components/shared/LeftSidebar"
+import RightSidebar from "@/components/shared/RightSidebar"
+import BottomBar from "@/components/shared/BottomBar"
 import '../globals.css'
 
 import type { Metadata } from 'next'
@@ -17,13 +21,19 @@ function RootLayout({ children }: { children: React.ReactNode }) {
       <ClerkProvider>
          <html lang="en">
             <body className={inter.className}>
+               <TopBar />
+
                <main>
+                  <LeftSidebar />
                   <section className="main-container">
                      <div className="w-full max-w-4x1">
                         {children}
                      </div>
                   </section>
+                  <RightSidebar />
                </main>
+
+               <BottomBar />
             </body>
          </html>
       </ClerkProvider>
